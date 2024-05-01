3ef34512d1e80d53d1142a6bf8184f8e873b6ab7
Milenta <marko1milentijevic@gmail.com>
2024-05-01 13:09:35 +0200

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Game of Dice.
+When you get number 6 you get special message that contains number you got.
+For all other numbers you get same message and number you got.
+Possible numbers are from 1 to 6
--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 "use strict";
 
-/* Game of Dice. When you get number 6 you get special message that contains number you got. 
-For all other numbers you get same message and number you got. 
-Possible numbers are from 1 to 6 */
 function getRandomIntDice() {
   return Number(Math.floor(Math.random() * 6 + 1));
 }
