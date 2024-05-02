af276f513b39263eb4ccef59b6be5cd9bfab9cbf
Milenta <marko1milentijevic@gmail.com>
2024-05-02 21:47:29 +0200

    commit

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const diceNumber = getRandomIntDice();
 console.log(typeof diceNumber);
 
 if (diceNumber === 6) {
-  document.getElementById("h1-el").innerText = `Congratulations you got 6.`;
+  document.getElementById("h3-el").innerText = `Congratulations you got 6.`;
 } else {
   document.getElementById(
-    "h1-el"
+    "h3-el"
   ).innerText = `Number you got is ${diceNumber}.`;
 }
