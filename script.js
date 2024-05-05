1a572487aa56f59237b7cda48f9be99db7753385
Milenta <marko1milentijevic@gmail.com>
2024-05-05 22:45:40 +0200

    commit

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 "use strict";
 
 var btn = document.querySelector("#twofuns");
-btn.addEventListener("click", method1);
+btn.addEventListener("click", rollDice);
+let count = 0;
+let disp = document.getElementById("display");
 
-function method1() {
+function rollDice() {
   function getRandomIntDice() {
     return Number(Math.floor(Math.random() * 6 + 1));
   }
   const diceNumber = getRandomIntDice();
   if (diceNumber === 6) {
-    document.getElementById("h3-el").innerText = `Congratulations you got 6.`;
+    document.getElementById(
+      "h3-el"
+    ).innerText = `Congratulations you got 6 from ${count + 1} clicks.`;
   } else {
     document.getElementById(
       "h3-el"
     ).innerText = `Number you got is ${diceNumber}.`;
   }
 }
+
+btn.onclick = function () {
+  count++;
+  // disp.innerHTML = count;
+};
