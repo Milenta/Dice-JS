f362837920c77a375af6c7bd44380c53e9a8896e
Milenta <marko1milentijevic@gmail.com>
2024-05-05 22:19:44 +0200

    commit

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 "use strict";
 
-function getRandomIntDice() {
-  return Number(Math.floor(Math.random() * 6 + 1));
-}
+var btn = document.querySelector("#twofuns");
+btn.addEventListener("click", method1);
 
-const diceNumber = getRandomIntDice();
-console.log(typeof diceNumber);
-
-if (diceNumber === 6) {
-  document.getElementById("h3-el").innerText = `Congratulations you got 6.`;
-} else {
-  document.getElementById(
-    "h3-el"
-  ).innerText = `Number you got is ${diceNumber}.`;
+function method1() {
+  function getRandomIntDice() {
+    return Number(Math.floor(Math.random() * 6 + 1));
+  }
+  const diceNumber = getRandomIntDice();
+  if (diceNumber === 6) {
+    document.getElementById("h3-el").innerText = `Congratulations you got 6.`;
+  } else {
+    document.getElementById(
+      "h3-el"
+    ).innerText = `Number you got is ${diceNumber}.`;
+  }
 }
